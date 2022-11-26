9ac3d10955c14a6b8222e000a14357ce8fb71976
kurekszymon <contact.kurek@gmail.com>
2022-11-26 16:30:27 +0100

    added input component to introduction module

--- webapp/src/components/Introduction/Introduction.jsx
+++ webapp/src/components/Introduction/Introduction.jsx
@@ -3,25 +3,45 @@ import "./Introduction.css";
 
 export default function Introduction() {
   const [isButtonClicked, setIsButtonClicked] = useState(false);
+  const [userInput, setUserInput] = useState("");
+
+  const handleChange = (e) => {
+    setUserInput(e.target.value);
+  };
 
   return (
-    <div className="module__introduction">
-      <div className="module__introduction--text">
-        <p>Hey!</p>
-        Try to<code> cy.get </code>me
+    <>
+      <div className="module__introduction">
+        {isButtonClicked ? (
+          <>
+            <p className="success-message">You did it!</p>
+            <input
+              className="example-input"
+              value={userInput}
+              placeholder={"Now fill up this input"}
+              onChange={handleChange}
+            />
+            <p className="typed-message">
+              You typed: {userInput ? userInput : "nothing yet :/"}
+            </p>
+          </>
+        ) : (
+          <>
+            <div className="module__introduction--text">
+              <p>Hey!</p>
+              Try to<code> cy.get </code>me
+            </div>
+            <div className="module__introduction--button">
+              <button onClick={() => setIsButtonClicked(true)}>
+                <pre>
+                  <code> Click me </code>
+                </pre>
+              </button>
+              <p>and see what happens!</p>
+            </div>
+          </>
+        )}
       </div>
-      <div className="module__introduction--button">
-        <button onClick={() => setIsButtonClicked(true)}>
-          <pre>
-            <code> Click me </code>
-          </pre>
-        </button>
-        <p>
-          {!isButtonClicked
-            ? "and see what happens!"
-            : "You did it! Choose another module :)"}
-        </p>
-      </div>
-    </div>
+    </>
   );
 }
